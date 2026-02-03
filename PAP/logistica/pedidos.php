1540e6ff0776d1fdd748934ed4897d0ab8188640
LRocha <lucassouza33441859@gmail.com>
2026-02-03 13:16:37 +0000

    Improve logistics layout and data integrity docs

--- PAP/logistica/pedidos.php
+++ PAP/logistica/pedidos.php
@@ -16,37 +16,57 @@ $pedidos = $conn->query("
 <head>
     <meta charset="UTF-8">
     <title>Pedidos - Logística</title>
-    <link rel="stylesheet" href="../css/pedidos.css">
+    <link rel="stylesheet" href="../assets/css/dashboard_log.css">
 </head>
 <body>
 
-<h1>Pedidos Recebidos</h1>
+<div class="layout">
+    <?php include 'sidebar.php'; ?>
 
-<table border="1" cellpadding="8">
-    <tr>
-        <th>ID</th>
-        <th>Entidade</th>
-        <th>Data</th>
-        <th>Estado</th>
-        <th>Ação</th>
-    </tr>
+    <main class="content">
+        <header class="topbar">
+            <h1>Pedidos Recebidos</h1>
+            <div class="user-info">👤 <?php echo htmlspecialchars($_SESSION['user_nome']); ?></div>
+        </header>
 
-    <?php while ($p = $pedidos->fetch_assoc()): ?>
-    <tr>
-        <td>#<?php echo $p['id']; ?></td>
-        <td><?php echo $p['entidade']; ?></td>
-        <td><?php echo $p['data_pedido']; ?></td>
-        <td><?php echo ucfirst($p['estado']); ?></td>
-        <td>
-            <a href="definir_precos.php?pedido_id=<?php echo $p['id']; ?>">
-                Analisar
-            </a>
-        </td>
-    </tr>
-    <?php endwhile; ?>
-</table>
-
-<a href="dashboard.php">Voltar</a>
+        <section class="table-wrapper">
+            <?php if ($pedidos->num_rows === 0): ?>
+                <p>Não existem pedidos pendentes.</p>
+            <?php else: ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Entidade</th>
+                            <th>Data</th>
+                            <th>Estado</th>
+                            <th>Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($p = $pedidos->fetch_assoc()): ?>
+                            <tr>
+                                <td>#<?php echo htmlspecialchars($p['id']); ?></td>
+                                <td><?php echo htmlspecialchars($p['entidade']); ?></td>
+                                <td><?php echo htmlspecialchars($p['data_pedido']); ?></td>
+                                <td>
+                                    <span class="badge-status pendente">
+                                        <?php echo htmlspecialchars(ucfirst(str_replace('_', ' ', $p['estado']))); ?>
+                                    </span>
+                                </td>
+                                <td>
+                                    <a class="btn link" href="definir_precos.php?pedido_id=<?php echo urlencode($p['id']); ?>">
+                                        Analisar
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            <?php endif; ?>
+        </section>
+    </main>
+</div>
 
 </body>
 </html>
